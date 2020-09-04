e1e8a451cf52dba3932f8c98864379d86f9cfeba
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-04 19:32:43 +0200

    Add the render method of the channellist component

--- components/channels/ChannelList.jsx
+++ components/channels/ChannelList.jsx
@@ -4,10 +4,26 @@ import Channel from './Channel.jsx'
 
 
 class ChannelList extends Component{
-
+    render(){
+        return(
+           <ul>
+               {this.props.channels.map(chan=>{
+                   <li>
+                       <Channel
+                        channel={chan}
+                        setChannel={this.props.setChannel}
+                        />
+                   </li>
+               })}
+           </ul> 
+        )
+    }
 }
 
 ChannelList.propTypes={
     channels: PropTypes.array.isRequired,
     setChannel: PropTypes.func.isRequired
 }
+
+
+
