d1583d34128b15dd3ee9c577a1d6ac7608792609
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-04 19:48:53 +0200

    Add onSubmit func

--- components/channels/ChannelForm.jsx
+++ components/channels/ChannelForm.jsx
@@ -2,7 +2,24 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 class ChannelForm extends Component{
-
+    onSubmit(e){
+        e.preventDefault();
+        const node = this.refs.channel;
+        const channelName=node.value;
+        this.props.addChannel(channelName)
+        node.value="";
+    }
+    render(){
+        return(
+            <form onSubmit={this.onSubmit.bind(this)}>
+                <input
+                    type="text"
+                    placeholder="Channel name"
+                    ref="channel"
+                />
+            </form>
+        )
+    }
 }
 
 ChannelForm.propTypes={
