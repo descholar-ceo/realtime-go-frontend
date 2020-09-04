de52503894412a4c5efc7fba1401d6a75dab1d36
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-04 19:40:04 +0200

    Add proptypes to the channelform component

---
+++ components/channels/ChannelForm.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class ChannelForm extends Component{
+
+}
+
+ChannelForm.propTypes={
+    addChannel: PropTypes.func.isRequired
+}
