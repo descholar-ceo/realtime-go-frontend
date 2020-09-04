a81c44437d05d1640f5dbdb9f5c7583fe2010547
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-04 23:50:28 +0200

    Finish the stylings

--- components/channels/ChannelSection.jsx
+++ components/channels/ChannelSection.jsx
@@ -6,9 +6,14 @@ import PropTypes from "prop-types";
 class ChannelSection extends Component{
     render(){
         return(
-            <div>
-                <ChannelList {...this.props} />
-                <ChannelForm {...this.props} />
+            <div className="support panel panel-primary">
+                <div className="panel-heading">
+                    <strong>Channels</strong>
+                </div>
+                <div className="channel-body channels">
+                    <ChannelList {...this.props} />
+                    <ChannelForm {...this.props} />
+                </div>
             </div>
         )
     }
