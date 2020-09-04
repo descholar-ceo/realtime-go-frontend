d9614df61995768e460fe0debe5a437b0d16387c
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-04 19:20:19 +0200

    Add the channel desi

--- components/channels/Channel.jsx
+++ components/channels/Channel.jsx
@@ -1,5 +1,29 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
-class Channel extends Component{}
+class Channel extends Component{
+    onClick(e){
+        e.preventDefault();
+        const {setChannel,channel}=this.props;
+        setChannel(channel);
+    }
+    render(){
+        const {channel}=this.props
+        return(
+            <ul>
+                <li>
+                    <a href="#" onClick={this.onClick.bind(this)}>
+                        {channel.name}
+                    </a>
+                </li>
+            </ul>
+        )
+    }
+}
 
-Channel.propTypes={}
+Channel.propTypes={
+    channel: PropTypes.object.isRequired,
+    setChannel: PropTypes.func.isRequired
+}
+
+export default Channel;
