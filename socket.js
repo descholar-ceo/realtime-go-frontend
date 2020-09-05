01b7fce0e448133e47b6520ed9d98d35ee7131bf
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-05 13:31:08 +0200

    Add Socket class and its constructor

--- socket.js
+++ socket.js
@@ -1,1 +1,9 @@
 import { EventEmitter } from "events";
+
+class Socket {
+    constructor(websocket=new WebSocket(),eventEmitter=new EventEmitter()){
+        super();
+        this.websocket=websocket;
+        this.eventEmitter=eventEmitter;
+    }
+}
