7ddd3108f618ed675291b5c302bd05f966acec04
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-05 13:46:59 +0200

    Remove super keyword

--- socket.js
+++ socket.js
@@ -2,7 +2,6 @@ import { EventEmitter } from "events";
 
 class Socket {
     constructor(websocket=new WebSocket(),eventEmitter=new EventEmitter()){
-        super();
         this.websocket=websocket;
         this.eventEmitter=eventEmitter;
         ws.onmessage=this.message.bind(this)
