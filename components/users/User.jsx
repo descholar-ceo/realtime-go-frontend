b0836aea1d11757fa042c5c5b408a0b59b0974eb
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-05 10:04:20 +0200

    Add export import user componet

--- components/users/User.jsx
+++ components/users/User.jsx
@@ -13,4 +13,6 @@ class User extends Component{
 
 User.propTypes={
     user:PropTypes.object.isRequired
-}
+};
+
+export default User;
--- components/users/UserList.jsx
+++ components/users/UserList.jsx
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import User from './User.jsx';
 
 class UserList extends Component{
-
+    render(){
+        return()
+    }
 }
 
 UserList.propTypes={
