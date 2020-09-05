639ac8bfac4be3aa6e6806568f0b2c0fd83c5578
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-05 11:09:28 +0200

    Add message list to the message section

--- components/messages/MessageSection.jsx
+++ components/messages/MessageSection.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import MessageForm from "./MessageForm.jsx";
-import ChannelList from "./ChannelList.jsx";
+import MessageList from "./MessageList.jsx";
 import PropTypes from "prop-types";
 
 class MessageSection extends Component{
@@ -11,7 +11,7 @@ class MessageSection extends Component{
                     <strong>Channels</strong>
                 </div>
                 <div className="panel-body channels">
-                    <ChannelList {...this.props} />
+                    <MessageList {...this.props} />
                     <MessageForm {...this.props} />
                 </div>
             </div>
