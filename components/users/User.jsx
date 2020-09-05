98e23e631069858d8413c9cfa3e51ae71761b960
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-05 09:52:16 +0200

    Add the render part of the user compoonent

--- components/users/User.jsx
+++ components/users/User.jsx
@@ -2,7 +2,13 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 class User extends Component{
-    
+    render(){
+        return(
+            <li>
+                {this.props.user.name}
+            </li>
+        );
+    }
 }
 
 User.propTypes={
