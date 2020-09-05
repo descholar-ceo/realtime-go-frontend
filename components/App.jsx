fd54420a71a713961ee95f1d202a9f984ad03151
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-05 13:02:14 +0200

    Change the event name

--- components/App.jsx
+++ components/App.jsx
@@ -24,7 +24,7 @@ class App extends Component{
 
     message(e){
         const event = JSON.parse(e.data)
-        if(event.name==='add channel'){
+        if(event.name==='channel add'){
             this.newChannel(event.data)
         }
     }
