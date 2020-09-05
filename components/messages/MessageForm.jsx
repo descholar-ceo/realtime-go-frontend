79e7f53d198638371b4e579466c4e71426d4ee01
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-05 12:34:37 +0200

    Make the id back

--- components/messages/MessageForm.jsx
+++ components/messages/MessageForm.jsx
@@ -11,7 +11,7 @@ class MessageForm extends Component{
     }
     render(){
         let input;
-        if(this.props.activeChannel!==undefined){
+        if(this.props.activeChannel.id!==undefined){
             input=(
                     <input
                         type="text"
