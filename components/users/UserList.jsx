e4f23b89c62f5c5076322fd5745e2501b0d8de43
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-05 10:03:04 +0200

    Add UserList component skeleton

---
+++ components/users/UserList.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class UserList extends Component{
+
+}
+
+UserList.propTypes={
+    users: PropTypes.array.isRequired
+}
